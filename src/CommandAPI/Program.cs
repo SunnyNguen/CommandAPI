964b3ab8868779403690a59feb5b66f49531da55
Sunny Nguen <sunnynguen22@gmail.com>
2023-10-03 14:11:03 +0300

    Initial Commit

---
+++ src/CommandAPI/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+namespace CommandAPI
+{
+ public class Startup
+ {
+ public void ConfigureServices(IServiceCollection services)
+ {
+ //SECTION 1. Add code below
+ services.AddControllers();
+ }
+ public void Configure(IApplicationBuilder app,
+IWebHostEnvironment env)
+ {
+ if (env.IsDevelopment())
+ {
+ app.UseDeveloperExceptionPage();
+ }
+ app.UseRouting();
+ app.UseEndpoints(endpoints =>
+ {
+ //SECTION 2. Add code below
+ endpoints.MapControllers();
+ });
+ }
+ }
+}
