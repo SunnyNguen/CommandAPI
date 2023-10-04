1a02ad63a01dc2b34b3f333f580cb4814f60d188
Sunny Nguen <sunnynguen22@gmail.com>
2023-10-04 11:36:14 +0300

    Added Command Model to API Project

---
+++ src/CommandAPI/Models/Command.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+namespace CommandAPI.Models
+{
+ public class Command
+ {
+ [Key]
+ [Required]
+ public int Id {get; set;}
+ [Required]
+ [MaxLength(250)]
+ public required string HowTo {get; set;}
+ [Required]
+ public required string Platform {get; set;}
+ [Required]
+ public required string CommandLine {get; set;}
+ }
+}
